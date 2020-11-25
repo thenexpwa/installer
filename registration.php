ec00620b870e21f5fd200186751f52f29671aaaf
Jayanka <jayan@codilar.com>
2020-11-26 01:02:05 +0530

    base theme added

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'NexPWA_Layout',
+    'NexPWA_Installer',
     __DIR__
 );
